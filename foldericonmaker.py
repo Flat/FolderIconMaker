45c01d0373dcc85216b69bfa0548f280e3163c74
Zaexu <Kitsune1911@gmail.com>
2013-06-28 16:08:28 -0700

    Update foldericonmaker.py
    Added conversion to .ico using imagemagick

--- foldericonmaker.py
+++ foldericonmaker.py
@@ -5,6 +5,7 @@ import os
 import sys
 import shutil
 from PIL import ImageOps
+from subprocess import Call
 
 #portrait dimensions default 160x228
 width = 160
@@ -16,6 +17,12 @@ if not os.path.exists("source/"):
 	print "Source folder not found. Created new folder. Please add source images to the source folder"
 	os.mkdir("source/")
 	sys.exit(3)
+if not os.path.exists("temp/")"
+	os.mkdir("temp/")
+else:
+	#if directory exists remove leftovers
+	shutil.rmtree("temp/")
+	os.mkdir("temp/")
 if not os.path.exists("output/"):
 	os.mkdir("output/")
 if not os.path.exists("used/"):
@@ -37,7 +44,14 @@ for filename in os.listdir('source'):
 	imgTop_w, imgTop_h=imgTop.size
 	offset=((imgBG_w-imgTop_w)/2, (imgBG_h-imgTop_h)/2)
 	imgBG.paste(imgTop,offset, imgTop)
-	imgBG.save('output/' + filename.rsplit(".",1)[0] + '.png')
+	imgBG.save('temp/' + filename.rsplit(".",1)[0] + '.png')
 	shutil.move("source/" + filename, "used/" + filename)
 	print filename + " Done!"
-print "Process complete! Completed files are in the output directory.\n"
+print "Compositing complete! Files are now undergoing conversion to .ico\n"
+for tempFile in os.listdir('temp'):
+	call(["convert","temp/" + filename + " output/" + filename.rsplit(".",1)[0] + '.ico'])
+	print filename + " Done!"
+print "Cleaning temp directory..."
+shutil.rmtree("temp/")
+print "Done!\n"
+print "Successfully completed conversion from .png to .ico! Files are in the output/ directory.\n"
